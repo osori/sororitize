1bbe5c5e75c402ea1431536048dba20d5e44aa60
Kevin <kmui2@wisc.edu>
2017-05-05 22:00:10 +0000

    fixed awkward spacing after punctuation. added autoremove redundant spacing. correctly parsed objects (before: <object at 0xXX really... >; now: really...)

--- sororitize.py
+++ sororitize.py
@@ -13,18 +13,21 @@ random_guy = random.choice(names.words('male.txt'))
 sorority_adverbs = ["literally", "totally", "seriously", "definitely", "kinda"]
 sorority_adverbs2 = ["fucking", "goddamn"]
 sorority_interjections = ["Oh my gaaahd, "+random_girl + "! ", "O. M. G. Look at her butt. ", "I mean... ", random_girl + "! My cute girl! ", "Waaaaaait. ", "Oh no. ", "Wait. What? ", "Wait. Whaaaaat? "]
-sorority_sentences = ['Seriously.', 'Oh wait.', random_girl + ', you are my bestie <3', 'Never mind, I am fine.', 'My heart just stopped for you!', random_girl + ' is such a bitch.', 'Wait, did you see ' + random_girl + '\'s Instagram post?', 'My mom is a total bitch.', "Let's go to Starbucks.", "Eww, " + random_guy + " is so gross.", "Chai tea latte just does not have calories.", "Let me change my political view to vegan.", "I am dating a beta named "+ random_guy + ". He is hot as hell.", "Are we pregaming at " + random_girl +" \'s?", "I am soooooo drunk.", "My iPhone is broken.      oh never mind", random_girl + ", you have to DTR right now."]
+sorority_sentences = ['Seriously.', 'Oh wait.', random_girl + ', you are my bestie <3', 'Never mind, I am fine.', 'My heart just stopped for you!', random_girl + ' is such a bitch.', 'Wait, did you see ' + random_girl + '\'s Instagram post?', 'My mom is a total bitch.', "Let's go to Starbucks.", "Eww, " + random_guy + " is so gross.", "Chai tea latte just does not have calories.", "Let me change my political view to vegan.", "I am dating a beta named "+ random_guy + ". He is hot as hell.", "Are we pregaming at " + random_girl +"\'s?", "I am soooooo drunk.", "My iPhone is broken.      oh never mind", random_girl + ", you have to DTR right now."]
 
 # sororitize function
 # accepts 
 def sororitize(text):
 	# POS Tagging the given text
-	tokenized = nltk.word_tokenize(str(text))
-	pos_tagged = iter(nltk.pos_tag(tokenized))
+	# tokenized = nltk.word_tokenize(str(text))
+	# print (text)
+	tokenized = list(filter(None, text.split(' ')))
+	# print (tokenized)
+	pos_tagged = nltk.pos_tag(tokenized)
 
 	# Like, you know, they always start with interjections like "Oh my gaaaahd"
 	new_sentence = random.choice(sorority_interjections)
-
+	
 	# Now sororitize every word
 	for word in pos_tagged:
 		# Insert sorority adverbs that modifies verbs (e.g. literally, totally)
@@ -57,19 +60,22 @@ def sororitize(text):
 		else: nochange = True
 
 		# insert space after each word
+		# if (word[1] == '.' or word[1] == '!' or word[1] == ',' or word[1] == "'"):
+		# 	new_sentence = new_sentence[:-1]
 		if (nochange): 
 			new_sentence = new_sentence + word[0]
 		new_sentence = new_sentence + ' '
 
 	return new_sentence
 
-# Change the text inside to try other inputs
-sorority = sororitize("I want to get you a chai tea latte to be your best friend. Let's go to Starbucks and have some fun yeaaaah.")
-
-# Insert random phrases after the sororitized text, making the text even more sororitized
-chance = random.randint(1,100)
-if (chance <= 90):
-	sorority = sorority + ' ' + sororitize(iter(nltk.pos_tag(nltk.word_tokenize(random.choice(sorority_sentences)))))
-
-# Demo: Printing the sororitized sentence
-print (sorority)
+for i in range(100):
+	# Change the text inside to try other inputs
+	sorority = sororitize("I want to get you a chai tea latte to be your best friend. Let's go to Starbucks and have some fun yeaaaah.")
+	
+	# Insert random phrases after the sororitized text, making the text even more sororitized
+	chance = random.randint(1,100)
+	if (chance <= 90):
+		sorority = sorority + sororitize(random.choice(sorority_sentences))
+	
+	# Demo: Printing the sororitized sentence
+	print (str(sorority))
